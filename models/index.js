16e6fa045dcc84a43c8920e75bbf2b330a81d6ba
slie07 <liestevano@gmail.com>
2018-02-22 12:01:41 -0700

    heroku

--- models/index.js
+++ models/index.js
@@ -1,2 +1,3 @@
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost/pokemon');
+
+mongoose.connect("mongodb://heroku_f0t92ddd:621v7j0p3d51orobqfbva08vkv@ds147118.mlab.com:47118/heroku_f0t92ddd" || 'mongodb://localhost/pokemon');
